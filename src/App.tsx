d53570d3c1d1df28ba3b45a45cc6cfe2cd03a2d3
cktang88 <arctangent20@yahoo.com>
2022-01-15 15:57:04 -0600

    use consistent nuber formatting

--- src/App.tsx
+++ src/App.tsx
@@ -91,10 +91,15 @@ enum Positive {
 
 function prettifyVal(val: string): [string, Positive] {
   try {
-    const n = Number(val);
+    let n = Number(val);
+    if (Number.isNaN(n)) {
+      throw Error("Not a number");
+    }
     const pos = n == 0 ? Positive.NONE : n > 0 ? Positive.TRUE : Positive.FALSE;
-    return [humanFormat(n).replace("G", "B"), pos]; // billion
+    n = Math.round(n / 1_000_000);
+    return [n.toLocaleString("en-US") + " M", pos]; // billion
   } catch (e) {
+    val = val == "None" ? "-" : val;
     return [val, Positive.NONE];
   }
 }
