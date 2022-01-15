2ce724af6729b431be7dacc447d62ca21d4cdbec
cktang88 <arctangent20@yahoo.com>
2022-01-15 13:25:22 -0600

    add caching

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,4 +1,11 @@
-export const run = () => {
-  console.log("hello");
-  console.log(import.meta.env.VITE_ALPHAVANTAGE_KEY);
+import alphavantage from "alphavantage";
+import { getBalanceSheet } from "./balanceSheet";
+
+export const alpha = alphavantage({
+  key: import.meta.env.VITE_ALPHAVANTAGE_KEY,
+});
+
+export const run = async () => {
+  const data = await getBalanceSheet("snow");
+  console.log(data);
 };
