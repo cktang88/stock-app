6084e58075d0cb5404c3a4d19090f238875838b1
cktang88 <arctangent20@yahoo.com>
2022-01-16 14:49:29 -0600

    add symbols

---
+++ src/symbols.ts
@@ -0,0 +1,24 @@
+export const SYMBOLS = [
+  "ibm",
+  "snow",
+  "aapl",
+  "crm", // salesforce
+  "afrm", // affirm
+  "mdb", //mongo
+  "ddog",
+  "net", //cloudflare
+  "axp", //amex
+  "v", //visa
+  "pypl", //paypal
+  "coin", //coinbase
+  "crwd", //crowdstrike
+  "zm", //zoom
+  "now", //servicenow
+  "okta",
+  "bill", //bill.com
+  "u", //unity
+  "rblx", //roblox
+  "msft",
+  "googl",
+  "fb",
+];
